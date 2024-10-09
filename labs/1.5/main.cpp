a141bf19578e3c8133275925c67ef7af6850c425
Maestro-nk <bovito2005@gmail.com>
2024-10-09 14:01:13 +0300

    5002

--- labs/1.5/main.cpp
+++ labs/1.5/main.cpp
@@ -1,9 +1,8 @@
 /**
- * Done by:
- * Student Name: Severyn Kotyhoroshko
- * Student Group: 123
- * Lab 1.5
- */
+* Student Name: Torgoniy Bogdan
+* Student Group: 123
+* Lab 1.5
+*/
 
 #include <iostream>
 #include <cmath>
@@ -12,23 +11,33 @@ using namespace std;
 
 int main() {
     {
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
+        float a = 54.9;
+        float b = 82.1;
+        int c = 74;
+        int d = 74;
 
-        // <УЛО1> (<УЛО2> (A<ОВ1>B) <БЛО> (<УЛО3> (C<ОВ2>D)))
-        //  empty (  !    (a == b)    ^   (   !   (c != d)))
-        bool res = (!(a == b) ^ (!(c != d)));
-        cout << "res: " << boolalpha << res << endl;
+        bool res1 = !(!(a <= b) || (c == d));
+        cout << "res1: " << boolalpha << res1 << endl;
     }
-
     {
-        int a = 0;
+        int a = 49;
+        int b = 58;
+        float c = 8.8;
+        float d = 6.6;
 
-        bool res = !a;
-        cout << "res: " << boolalpha << res << endl;
+        bool res2 = !(!(a <= b) || (c == d));
+        cout << "res2: " << boolalpha << res2 << endl;
     }
+    {
+        int a = 41;
+        int b = -13;
+        int c = 22;
+        int d = -593;
+        int e = 12;
+        float f;
 
-    return 0;
+        f = (a & +b) / c > (d + e) << (sizeof(float) * 8);
+
+        std::cout << "Result: " << f << std::endl;
+    }
 }
